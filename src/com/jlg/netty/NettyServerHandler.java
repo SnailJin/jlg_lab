1d86d99b12e7666d08e1b8b85fcc4d9105e2a79f
SnailJin <jin521436@163.com>
2015-12-17 18:47:57 +0800

    2015-12-17   常规上传

--- src/com/jlg/netty/NettyServerHandler.java
+++ src/com/jlg/netty/NettyServerHandler.java
@@ -1,7 +1,32 @@
 package com.jlg.netty;
 
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class NettyServerHandler extends SimpleChannelHandler{
+
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+		// TODO Auto-generated method stub
+		super.messageReceived(ctx, e);
+		BaseMsg msg= (BaseMsg)e.getMessage();
+		Channel channel = e.getChannel();
+		switch (msg.getType()) {
+		case PING:
+			PingMsg pingMsg = (PingMsg)msg;
+			channel.write(pingMsg);
+			System.out.println("client"+msg.getClientId()+" ping成功");
+			break;
+		case LOGIN:
+			LoginMsg login = (LoginMsg)msg;
+			channel.write(login);
+			System.out.println("client"+msg.getClientId()+" 登录成功");
+			break;
+		}
+	}
 
 }
